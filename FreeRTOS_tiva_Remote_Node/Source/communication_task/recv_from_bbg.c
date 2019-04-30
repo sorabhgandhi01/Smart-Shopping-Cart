8248e2a940d195d8a3c6e05b1e860adcdf46e3b8
Sorabh Gandhi <sorabh.gandhi@colorado.edu>
2019-04-29 23:22:05 -0600

    adding degraded mode flag

--- FreeRTOS_tiva_Remote_Node/Source/communication_task/recv_from_bbg.c
+++ FreeRTOS_tiva_Remote_Node/Source/communication_task/recv_from_bbg.c
@@ -37,16 +37,16 @@ void recv_from_bbg_task(void *pvParameters)
 
 
     UARTprintf("***********RECV_FROM_BBG_TASK************\n\r");
-    TimerHandle_t xTimers_hb = xTimerCreate
-                        (
-                        "recv_from_bbg_task",
-                        pdMS_TO_TICKS(5000),
-                        pdTRUE,
-                        ( void * ) 0,
-                        vRecv_from_bbg_TimerCallback
-                        );
-
-        xTimerStart(xTimers_hb, 0);
+//    TimerHandle_t xTimers_hb = xTimerCreate
+//                        (
+//                        "recv_from_bbg_task",
+//                        pdMS_TO_TICKS(5000),
+//                        pdTRUE,
+//                        ( void * ) 0,
+//                        vRecv_from_bbg_TimerCallback
+//                        );
+//
+//        xTimerStart(xTimers_hb, 0);
 
 
     for (;;) {
@@ -56,10 +56,10 @@ void recv_from_bbg_task(void *pvParameters)
 
             UARTprintf("Rec %d\r\n", c);
 
-            if(c == 0x7e)
-            {
-                current_count++;
-            }
+//            if(c == 0x7e)
+//            {
+//                current_count++;
+//            }
         }
 
     }
